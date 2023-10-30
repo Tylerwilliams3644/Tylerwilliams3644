877afb1c269358f01905819125f0587eb53656fa
Tylerwilliams3644 <144622561+Tylerwilliams3644@users.noreply.github.com>
2023-10-29 20:18:39 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ with open("budget_data.csv", "r") as csv_file:
 
     next(csv_reader)
 
+    sum_profit_loss = []
+
     for line in csv_reader:
         profit_loss = line[1]
+        # print(int(profit_loss))
+        # print(type(int(profit_loss)))
+
+        sum_profit_loss.appent(line[1][int(profit_loss)])
+        print(sum_profit_loss)
         # dates = line[0]
         # print(profit_loss)
         # print(dates)
